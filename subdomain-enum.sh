42f144dca8b84a0ffc2fbc4fcbd7a578dfd0d6a8
Sahil Dari <52995565+sahildari@users.noreply.github.com>
2022-01-05 19:27:14 +0530

    Update subdomain-enum.sh

--- subdomain-enum.sh
+++ subdomain-enum.sh
@@ -8,23 +8,23 @@ YELLOW="\e[33m"
 
 
 ## running subfinder
-echo -ne "${CYAN} [+] Starting Subfinder${ENDCOLOR}\n"
+echo -e "${CYAN} [+] Starting Subfinder${ENDCOLOR}\n"
 subfinder -silent -d $1 -o domains.txt
-echo -ne "\n"
+echo -e "\n"
 
 #runing assetfinder
-echo -ne "${CYAN}[+] Starting assetfinder${ENDCOLOR}\n"
+echo -e "${CYAN}[+] Starting assetfinder${ENDCOLOR}\n"
 assetfinder --subs-only $1 | tee -a domains.txt
 
-echo -ne "\n"
+echo -e "\n"
 
 #running findomain-linux
-echo -ne "${CYAN}[+]Starting findomain-linux${ENDCOLOR}\n"
+echo -e "${CYAN}[+]Starting findomain-linux${ENDCOLOR}\n"
 findomain-linux --output --quiet --target $1
-echo -ne "\n"
+echo -e "\n"
 
-echo -ne "${GREEN}[+] Subdomain Enumeration completed${ENDCOLOR}"
-echo -ne "\n\n"
+echo -e "${GREEN}[+] Subdomain Enumeration completed${ENDCOLOR}"
+echo -e "\n\n"
 
 #removing dublicate entries
 cat "$1.txt" >> domains.txt
@@ -32,14 +32,12 @@ rm "$1.txt"
 
 sort -u domains.txt -o domains.txt
 
-echo -ne "${YELLOW}[+] All unique Subdomains have been written to domains.txt${ENDCOLOR}"
+echo -e "${YELLOW}[+] All unique Subdomains have been written to domains.txt${ENDCOLOR}"
 
-echo -ne "\n\n"
-echo -ne "${CYAN}[+] Checking for alive domains..${ENDCOLOR}\n"
+echo -e "\n\n"
+echo -e "${CYAN}[+] Checking for alive domains..${ENDCOLOR}\n"
 cat domains.txt | httprobe | tee -a alive.txt
 
 
 cat alive.txt | python3 -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > alive.json
 cat domains.txt | python3 -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > domains.json
-
-
