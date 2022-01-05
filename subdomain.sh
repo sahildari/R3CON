0b80dc688b302e46ffc49c6ecba149deb297e5a2
Sahil Dari <52995565+sahildari@users.noreply.github.com>
2022-01-05 19:29:45 +0530

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,29 +1,28 @@
-##!/bin/bash
+#!/bin/bash
+
 CYAN="\e[36m"
 ENDCOLOR="\e[0m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
 
-
-
 ## running subfinder
-echo "${CYAN}[+] Starting Subfinder${ENDCOLOR}"
+echo -e "${CYAN}[+] Starting Subfinder${ENDCOLOR}"
 subfinder -silent -d $1 -o domains.txt
-echo "\n"
+echo -e "\n"
 
 #runing assetfinder
-echo "${CYAN}[+] Starting assetfinder${ENDCOLOR}"
+echo -e "${CYAN}[+] Starting assetfinder${ENDCOLOR}"
 assetfinder --subs-only $1 | tee -a domains.txt
 
-echo "\n"
+echo -e "\n"
 
 #running findomain
-echo "${CYAN}[+]Starting findomain${ENDCOLOR}"
+echo -e "${CYAN}[+]Starting findomain${ENDCOLOR}"
 findomain --quiet --output --target $1
-echo "\n"
+echo -e "\n"
 
-echo "${GREEN}[+] Subdomain Enumeration completed${ENDCOLOR}"
-echo "\n\n"
+echo -e "${GREEN}[+] Subdomain Enumeration completed${ENDCOLOR}"
+echo -e "\n\n"
 
 ##removing dublicate entries
 cat "$1.txt" >> domains.txt
@@ -31,10 +30,10 @@ rm "$1.txt"
 
 sort -u domains.txt -o domains.txt
 
-echo "${YELLOW}[+] All unique Subdomains have been written to domains.txt${ENDCOLOR}"
+echo -e "${YELLOW}[+] All unique Subdomains have been written to domains.txt${ENDCOLOR}"
 
-echo "\n\n"
-echo "${CYAN}[+] Checking for alive domains..${ENDCOLOR}"
+echo -e "\n\n"
+echo -e "${CYAN}[+] Checking for alive domains..${ENDCOLOR}"
 cat domains.txt | httprobe | tee -a alive.txt
 
 
@@ -42,36 +41,36 @@ cat alive.txt | python -c "import sys; import json; print (json.dumps({'domains'
 cat domains.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > domains.json
 
 
-echo "${YELLOW}[+] Finishing things up.....${ENDCOLOR}"
+echo -e "${YELLOW}[+] Finishing things up.....${ENDCOLOR}"
 
-echo "\n"
+echo -e "\n"
 
-echo "${GREEN}[+] All Alive Subdomains have been written to alive.txt${ENDCOLOR}"
+echo -e "${GREEN}[+] All Alive Subdomains have been written to alive.txt${ENDCOLOR}"
 
-echo "\n"
+echo -e "\n"
 
-echo "=================================================================================================="
+echo -e "=================================================================================================="
 #calling cut.sh
 
-echo "\n\n"
-echo "${YELLOW}[+] Using cut.sh now.....${ENDCOLOR}"
+echo -e "\n\n"
+echo -e "${YELLOW}[+] Using cut.sh now.....${ENDCOLOR}"
 cut.sh alive.txt
 
-echo "=================================================================================================="
+echo -e "=================================================================================================="
 #calling jsfiles.sh
 
-echo "\n\n"
-echo "${YELLLOW}[+] Using the jsfiles.sh now .....${ENDCOLOR}"
+echo -e "\n\n"
+echo -e "${YELLLOW}[+] Using the jsfiles.sh now .....${ENDCOLOR}"
 jsfiles.sh alive.txt
 
-echo "=================================================================================================="
+echo -e "=================================================================================================="
 #calling endpoint.sh 
 
-echo "\n\n"
-echo "${YELLOW}[+] Using the endpoint.sh file now....${ENDCOLOR}"
+echo -e "\n\n"
+echo -e "${YELLOW}[+] Using the endpoint.sh file now....${ENDCOLOR}"
 endpoint.sh alive.txt
 
-echo "=================================================================================================="
+echo -e "=================================================================================================="
 
 
-echo "${GREEN}[+] EXECUTION COMPLETED ${ENDCOLOR}"
+echo -e "${GREEN}[+] EXECUTION COMPLETED ${ENDCOLOR}"
